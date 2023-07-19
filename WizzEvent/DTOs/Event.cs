b97ab96982b4489b1b3477a36e76c88da93e1395
Rolando Quirós Artvia <rjquiros21@gmail.com>
2023-07-18 21:07:41 -0600

    Creacion del DTO Evento

---
+++ WizzEvent/DTOs/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class Event:BaseDTO
+    {
+        public string Name { get; set; }
+        public string Slogan { get; set; }
+        public string Description { get; set; }
+        public string Modality { get; set; }
+        public DateTime EventDate { get; set; }
+        public int TotalTickets { get; set; }
+        public string Information { get; set; }
+        public string PaymentMethod { get; set; }
+        public int FreeTickets { get; set; }
+        public int OwnedBy { get; set; }
+        public string State { get; set; }
+        public int IdCategory { get; set; }
+    }
+}
+
+
